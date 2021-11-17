47111af95ab03fe20040a78d71e4fc47d6e69e61
HyungJooKim <hyungjookim12@gmail.com>
2021-11-17 14:45:26 +0900

    [2021-11-17 김형주] 1.초기 설정

---
+++ src/main/java/com/project/dataline/DataLineApplication.java
@@ -0,0 +1,13 @@
+package com.project.dataline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataLineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/dataline/DataLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dataline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
